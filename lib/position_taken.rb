234314926d6aa45d9a699319710171ab8d2e84ae
Tracy Reuther <tracyreuther@gmail.com>
2019-05-07 22:54:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if (board[index] == "X") || (board[index] == "O")
     true
-  elsif board[index] == nil 
+  elsif board[index] == nil
     false
   else (board[index] == " ") || (board[index])
     false
